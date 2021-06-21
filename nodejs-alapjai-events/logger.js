c9fa9bcbc24745bdb45c2f038fff01c15288911b
Vilhelmus25 <vorvie@gmail.com>
2021-06-21 22:53:33 +0200

    eventHome

---
+++ nodejs-alapjai-events/logger.js
@@ -0,0 +1,15 @@
+class Logger {
+    constructor() {
+
+    }
+
+    error(logType) {
+        console.log(logType);
+    }
+
+    success(logType) {
+        console.log(logType);
+    }
+}
+
+module.exports = Logger
