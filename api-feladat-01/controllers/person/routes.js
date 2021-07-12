1733efa746c7e70609d61498d0fcc4f3d1a11f23
Vilhelmus25 <vorvie@gmail.com>
2021-07-12 21:55:10 +0200

    API-1 done with no swagger

--- api-feladat-01/controllers/person/routes.js
+++ api-feladat-01/controllers/person/routes.js
@@ -4,9 +4,13 @@ const data = require('./db.json');
 const controller = express.Router();
 
 controller.get('/count', (req, res) => {
-    console.log(data.person);
     const filteredArray = data.person.filter((item) => item.vaccine !== "")
     res.json(`The number of veccinated persons in the database is: ${filteredArray.length}`);
+});
+
+controller.get('/vaccinated', (req, res) => {
+    const filteredArray = data.person.filter((item) => item.vaccine !== "")
+    res.json(`The people vaccinated are: ${JSON.stringify(filteredArray)}`);
 });
 
 module.exports = controller;
