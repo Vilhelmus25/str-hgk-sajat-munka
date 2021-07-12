f0e03102265314d6460e077d320240e6ae0d76cd
Vilhelmus25 <vorvie@gmail.com>
2021-07-12 21:43:43 +0200

    API 1 - first function

--- api-feladat-01/controllers/person/routes.js
+++ api-feladat-01/controllers/person/routes.js
@@ -3,7 +3,7 @@ const data = require('./db.json');
 
 const controller = express.Router();
 
-controller.get('/', (req, res) => {
+controller.get('/count', (req, res) => {
     console.log(data.person);
     const filteredArray = data.person.filter((item) => item.vaccine !== "")
     res.json(`The number of veccinated persons in the database is: ${filteredArray.length}`);
